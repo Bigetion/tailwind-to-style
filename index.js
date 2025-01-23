5fa308de46f9f61f76c45389d12c715a38d3fcd8
Bigetion <adysuprianto@yahoo.co.id>
2025-01-23 17:50:51 +0800

    update ring default variable

--- index.js
+++ index.js
@@ -4255,6 +4255,10 @@ function generator$I(configOptions = {}) {
       const key = keyTmp.toLowerCase() !== "default" ? `-${keyTmp}` : "";
       return `
           ${prefix}${key} {
+            --ring-inset: var(--empty,/*!*/ /*!*/);
+            --ring-offset-width: 0px;
+            --ring-offset-color: #fff;
+            --ring-color: rgba(59, 130, 246, 0.5);
             --ring-offset-shadow: var(--ring-inset) 0 0 0 var(--ring-offset-width) var(--ring-offset-color);
             --ring-shadow: var(--ring-inset) 0 0 0 calc(${value} + var(--ring-offset-width)) var(--ring-color);
             box-shadow: var(--ring-offset-shadow), var(--ring-shadow);
