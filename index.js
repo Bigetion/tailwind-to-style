7ca25693966d83658268c72b9a717e4710d182b4
Bigetion <shinichi.dconnan@gmail.com>
2025-06-12 20:17:59 +0800

    Remove export tailwindCss raw and object

--- index.js
+++ index.js
@@ -1630,34 +1630,37 @@ function generator$2i(configOptions = {}) {
 
   const { backgroundColor = {} } = theme;
 
-  const responsiveCssString = generateCssString(({ getCssByColors, isValidCssColor }) => {
-    const cssString = getCssByColors(
-      backgroundColor,
-      (key, value, rgbValue) => {
-        let rgbPropertyValue = "";
-        if (rgbValue) {
-          rgbPropertyValue = `background-color: rgba(${rgbValue}, var(--bg-opacity));`;
-        }
+  const responsiveCssString = generateCssString(
+    ({ getCssByColors, isValidCssColor }) => {
+      const cssString = getCssByColors(
+        backgroundColor,
+        (key, value, rgbValue) => {
+          let rgbPropertyValue = "";
+          if (rgbValue) {
+            rgbPropertyValue = `background-color: rgba(${rgbValue}, var(--bg-opacity));`;
+          }
 
-        if (value === "custom_value") {
-          return `
+          if (value === "custom_value") {
+            return `
             ${prefix}-${key} {
-              ${isValidCssColor(value) ? "background-color" : "background"}: ${value};
+              ${
+                isValidCssColor(value) ? "background-color" : "background"
+              }: ${value};
             }
           `;
-        }
+          }
 
-        return `
+          return `
             ${prefix}-${key} {
               --bg-opacity: 1;
               background-color: ${value};
               ${rgbPropertyValue}
             }
           `;
-      }
-    );
-    return cssString;
-  });
+        }
+      );
+      return cssString;
+    });
 
   return responsiveCssString;
 }
@@ -6319,11 +6322,5 @@ function twsx(obj) {
   return cssString.trim();
 }
 
-const tailwindCss = {
-  raw: twString,
-  object: cssObject,
-};
-
-exports.tailwindCss = tailwindCss;
 exports.tws = tws;
 exports.twsx = twsx;
