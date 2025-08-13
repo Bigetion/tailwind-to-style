7915c06a3c962773c5d5d4127ab03647a2d79843
Bigetion <adysuprianto@yahoo.co.id>
2025-08-13 13:09:59 +0700

    Update plugin

--- plugins/vite-twsx.js
+++ plugins/vite-twsx.js
@@ -3,26 +3,31 @@ const path = require("path");
 const { twsx } = require("tailwind-to-style");
 
 function buildTwsx(twsxDir, outDir) {
-  let allCss = '';
+  let allCss = "";
   fs.readdirSync(twsxDir).forEach((file) => {
-    if (file.endsWith('.json')) {
-      const json = JSON.parse(fs.readFileSync(path.join(twsxDir, file), 'utf8'));
+    if (file.endsWith(".json")) {
+      const json = JSON.parse(
+        fs.readFileSync(path.join(twsxDir, file), "utf8")
+      );
       const css = twsx(json, { inject: false });
-      const outFile = path.join(outDir, file.replace('.json', '.css'));
+      const outFile = path.join(outDir, file.replace(".json", ".css"));
       fs.writeFileSync(outFile, css);
-      allCss += css + '\n';
+      allCss += css + "\n";
     }
   });
   return allCss;
 }
 
 module.exports = function twsxPlugin(options = {}) {
-  const twsxDir = options.twsxDir || path.resolve(process.cwd(), 'src/twsx');
-  const outDir = options.outDir || path.resolve(process.cwd(), 'dist');
-  const cssFile = path.resolve(process.cwd(), 'node_modules/tailwind-to-style/twsx.css');
+  const twsxDir = options.twsxDir || path.resolve(process.cwd(), "src/twsx");
+  const outDir = options.outDir || path.resolve(process.cwd(), "dist");
+  const cssFile = path.resolve(
+    process.cwd(),
+    "node_modules/tailwind-to-style/twsx.css"
+  );
 
   return {
-    name: 'vite-twsx',
+    name: "vite-twsx",
     buildStart() {
       const allCss = buildTwsx(twsxDir, outDir);
       fs.writeFileSync(cssFile, allCss);
--- plugins/webpack-twsx.js
+++ plugins/webpack-twsx.js
@@ -1,35 +1,39 @@
-
-const fs = require('fs');
-const path = require('path');
-const { twsx } = require('tailwind-to-style');
+const fs = require("fs");
+const path = require("path");
+const { twsx } = require("tailwind-to-style");
 
 class TwsxPlugin {
   constructor(options = {}) {
-    this.twsxDir = options.twsxDir || path.resolve(process.cwd(), 'src/twsx');
-    this.outDir = options.outDir || path.resolve(process.cwd(), 'dist');
+    this.twsxDir = options.twsxDir || path.resolve(process.cwd(), "src/twsx");
+    this.outDir = options.outDir || path.resolve(process.cwd(), "dist");
   }
 
   buildTwsx() {
-    let allCss = '';
+    let allCss = "";
     fs.readdirSync(this.twsxDir).forEach((file) => {
-      if (file.endsWith('.json')) {
-        const json = JSON.parse(fs.readFileSync(path.join(this.twsxDir, file), 'utf8'));
+      if (file.endsWith(".json")) {
+        const json = JSON.parse(
+          fs.readFileSync(path.join(this.twsxDir, file), "utf8")
+        );
         const css = twsx(json, { inject: false });
-        const outFile = path.join(this.outDir, file.replace('.json', '.css'));
+        const outFile = path.join(this.outDir, file.replace(".json", ".css"));
         fs.writeFileSync(outFile, css);
-        allCss += css + '\n';
+        allCss += css + "\n";
       }
     });
     return allCss;
   }
 
   apply(compiler) {
-    const cssFile = path.resolve(process.cwd(), 'node_modules/tailwind-to-style/twsx.css');
-    compiler.hooks.beforeCompile.tap('TwsxPlugin', () => {
+    const cssFile = path.resolve(
+      process.cwd(),
+      "node_modules/tailwind-to-style/twsx.css"
+    );
+    compiler.hooks.beforeCompile.tap("TwsxPlugin", () => {
       const allCss = this.buildTwsx();
       fs.writeFileSync(cssFile, allCss);
     });
-    compiler.hooks.watchRun.tap('TwsxPlugin', () => {
+    compiler.hooks.watchRun.tap("TwsxPlugin", () => {
       const allCss = this.buildTwsx();
       fs.writeFileSync(cssFile, allCss);
     });
